e4e59f26739cf43293f38d54257fa5ae98c0b259
shivamj2603 <shivamj2603@gmail.com>
2020-11-02 07:56:39 +0530

    Added:Query to create a table addressbook in database

--- addressBook.sql
+++ addressBook.sql
@@ -2,3 +2,18 @@
 create database addressBook_service;
 show databases;
 use addressBook_service;
+
+# Usecase 2:
+create table addressBook
+(
+firstName varchar(150) not null,
+lastName varchar(150) not null,
+address varchar(150) not null,
+city varchar(50) not null,
+state varchar(50) not null,
+zip numeric(6) not null,
+phone numeric(12) not null,
+email varchar(100) not null,
+primary key (phone)
+);
+describe addressBook;
